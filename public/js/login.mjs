d2123c8818ad7ef662a545b308c45647660c2949
Luis Lima <luisfernando.lima2005@gmail.com>
2024-11-13 21:21:07 -0300

    Criação da tela inicial (desktop)

---
+++ public/js/login.mjs
@@ -0,0 +1,4 @@
+import PasswordVisibility from "./class/PasswordVisibility.mjs";
+const inputPassword = new PasswordVisibility("#loginPassword", ".passwordViewButton")
+
+inputPassword.initializeChangeVisibility()
--- routes/router.mjs
+++ routes/router.mjs
@@ -1,8 +1,12 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index", {titulo: 'Login', css: "login.css"})
-})
+  res.render("index", { titulo: "Login", css: "login.css" });
+});
 
-export default router
+router.get("/login", (req, res) => {
+  res.render("login", { titulo: "Login", css: "login.css", js: "login.mjs" });
+});
+
+export default router;
