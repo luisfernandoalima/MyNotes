72244151486ffa67a3d932a1314fdac8c2f47704
Luis Lima <luisfernando.lima2005@gmail.com>
2024-11-18 15:15:44 -0300

    Criação da tela de notas concluídas

---
+++ public/js/completedNotes.mjs
@@ -0,0 +1,6 @@
+import CreateMenu from "./class/menuLateral.mjs";
+
+const Menu = new CreateMenu().initializeMenu()
+
+import CreatePageUpButton from "./class/PageUp.mjs";
+const PageUpButton = new CreatePageUpButton().initialize()
--- routes/router.mjs
+++ routes/router.mjs
@@ -2,7 +2,11 @@ import express from "express";
 const router = express.Router();
 
 router.get("/signin", (req, res) => {
-  res.render("signin", { titulo: "Cadastre-se", css: "signin.css", js: "signin.mjs" });
+  res.render("signin", {
+    titulo: "Cadastre-se",
+    css: "signin.css",
+    js: "signin.mjs",
+  });
 });
 
 router.get("/login", (req, res) => {
@@ -10,11 +14,35 @@ router.get("/login", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  res.render("index", {titulo: "MyNotes - Página Inicial", css: "paginaInicial.css", js: "paginaInicial.mjs"})
-})
+  res.render("index", {
+    titulo: "MyNotes - Página Inicial",
+    css: "paginaInicial.css",
+    js: "paginaInicial.mjs",
+  });
+});
 
-router.get("/new-note", (re1, res) => {
-  res.render("newNote", {titulo: "MyNotes - Nova Nota", css: 'newNote.css', js: "newNote.mjs"})
+router.get("/new-note", (req, res) => {
+  res.render("newNote", {
+    titulo: "MyNotes - Nova Nota",
+    css: "newNote.css",
+    js: "newNote.mjs",
+  });
+});
+
+router.get("/completed-notes", (req, res) => {
+  res.render("completedNotes", {
+    titulo: "MyNotes - Notas Concluídas",
+    css: "completedNotes.css",
+    js: "completedNotes.mjs",
+  });
+});
+
+router.get("/canceled-notes", (req, res) => {
+  res.render("canceledNotes", {
+    titulo: "MyNotes - Notas Canceladas",
+    css: "canceledNotes.css",
+    js: "canceledNotes.mjs" 
+  })
 })
 
 export default router;
